0ca658d8de0c3bafb0c5dd12446148e2342350b1
Rehaman-rafi <Rehamanrafi19@gmail.com>
2024-10-19 23:06:17 +0600

    js data structure

---
+++ script.js
@@ -0,0 +1,27 @@
+// index array......
+
+let marks = [56,78,90,12,34,53,60,11,20,57]
+console.log(marks[5]);
+console.log(marks[3]);
+console.log(marks[8]);
+
+// associative array..............
+
+let human = {firstname:"abdul", lastname:"jabbar", age:12, balance:40};
+console.log("firstname is =",human.firstname);
+console.log(human.lastname);
+console.log(human.age);
+console.log(human.balance);
+
+// two dimensional array.........
+
+let twodimensional =[
+    [10,3,4,45],
+    [45,7,9,90],
+    [12,34,56,78],
+    [45,65,32,10],
+]
+
+console.log(twodimensional[2][1]);
+console.log(twodimensional[0][3]);
+console.log(twodimensional[3][1]);
